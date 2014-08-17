97b3a8c2ef128f9660af56a2325f33bbb1ffcc20
mrmikemcguire <mcguire@mullenhigh.com>
2014-08-17 15:58:58 -0600

    Initial Commit

---
+++ GitHub Test/src/GitHubTest.java
@@ -0,0 +1,18 @@
+
+public class GitHubTest
+	{
+	public static void main(String[] args)
+		{
+		System.out.println("I downloaded this at school.");
+		
+		//Add a line about modifying it at school, then upload it
+		// back to GitHub.
+
+		//Open this at home and add a line about modifying it there.
+		//Update it on GitHub.
+		
+		//Open it up back at school to demonstrate you can move
+		//code back and forth and modify it both places.
+
+		}
+	}
